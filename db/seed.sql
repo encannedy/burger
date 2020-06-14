32d96e991881a241323e396d2b9fbd80e7406387
Erica Cannedy <encannedy@gmail.com>
2020-06-13 23:36:03 -0400

    updated schema and seed files

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (id, burger_name, devoured) VALUES (1, "Veggie", "false");
+INSERT INTO burgers (id, burger_name, devoured) VALUES (2, "Bison", "true");
+INSERT INTO burgers (id, burger_name, devoured) VALUES (3, "Tuna", "true");
+INSERT INTO burgers (id, burger_name, devoured) VALUES (4, "Beef", "false");
