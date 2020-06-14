3317787b3edf2eea38c4be73ff498d94a8a430e0
Erica Cannedy <encannedy@gmail.com>
2020-06-14 00:37:55 -0400

    server.js file added

---
+++ server.js
@@ -0,0 +1,19 @@
+  
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//Routes
+const routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: http://localhost:" + PORT);
+});
